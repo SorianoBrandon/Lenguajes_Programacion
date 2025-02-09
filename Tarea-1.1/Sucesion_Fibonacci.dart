cc457469d84654ec3df5831d13d45d4d099700c7
SorianoBrandon <bsorianoh@unah.hn>
2025-02-08 19:45:58 -0600

    Listo

--- Tarea-1.1/Sucesion_Fibonacci.dart
+++ Tarea-1.1/Sucesion_Fibonacci.dart
@@ -1,3 +1,19 @@
-void main{
-    print('Hola Mundo')
+void main(){
+    print("----------------- PRIMEROS 15 NUMEROS SUCESION DE FIBONACCI -----------------");
+    for (int i = -1; i < 13; i++) {
+        print(fibonacci(i));
+    }
+}
+
+int fibonacci(int n){
+    if(n<=1){
+      if(n==1){
+        print(n);
+        return n + 1;
+      }else{
+        return n+1;
+      }
+    }else{
+      return (n+(n-1));
+    }
 }
